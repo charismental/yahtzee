c75d0f36bd30e28657da597a6f7aeafc90dd97e0
Tristin <thewett-c@sbgtv.com>
2020-06-13 09:49:51 -0700

    Import vuetify 2.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,18 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
+export default Vue.extend({
   name: 'Home',
+
   components: {
     HelloWorld,
   },
-};
+
+  data: () => ({
+    //
+  }),
+});
 </script>
